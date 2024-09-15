1ad940b590c230c8b3b01f5f2834592eff0dbf06
Gabriel <127786524+gabrieljpeg@users.noreply.github.com>
2024-09-15 16:16:54 -0300

    Add files via upload

---
+++ soma.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+
+	int x, y;
+	int soma;
+
+	cout << "Foneça um número: ";
+	cin >> x;
+
+	cout << "Forneça outro número: ";
+	cin >> y;
+
+	soma = x + y;
+
+	cout << "Resultado da soma: " << soma;
+
+	return 0;
+}
